7484956c91fb5a730e35b3b1a5eb9fd3ce22d86e
justcppdev <justcppdev@ya.ru>
2017-12-28 16:55:17 +0300

    Initial Commit

---
+++ reverse_by_words/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  reverse_by_words
+//
+//  Created by simplydesigner on 28/12/2017.
+//  Copyright © 2017 simplydesigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
