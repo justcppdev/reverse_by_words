bb91f32956870062546356df1f4f4d8c3fc2c351
justcppdev <justcppdev@ya.ru>
2018-01-14 10:32:21 +0300

    init

--- reverse_by_words/main.cpp
+++ reverse_by_words/main.cpp
@@ -1,15 +1,40 @@
-//
-//  main.cpp
-//  reverse_by_words
-//
-//  Created by simplydesigner on 28/12/2017.
-//  Copyright © 2017 simplydesigner. All rights reserved.
-//
-
 #include <iostream>
 
+template <typename Iter>
+void reverse_by_symbol( Iter first, Iter last ) {
+    for( auto i = first, j = last; i != j; ++i ) {
+        --j;
+        std::iter_swap( i, j );
+        if( i == j ) {
+            break;
+        }
+    }
+}
+
+template< typename Iter >
+void reverse_by_words( Iter first, Iter last )
+{
+    reverse_by_symbol( first, last );
+    auto i = first, j = first;
+    for( ; i != last; ++i ) {
+        if( *j == ' ' ) {
+            j = i;
+        }
+        else if( *i == ' ' ) {
+            reverse_by_symbol( j, i );
+            j = i;
+        }
+    }
+    
+    reverse_by_symbol( j, i );
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    for( std::string line; std::getline( std::cin , line ); ) {
+        reverse_by_words( line.begin(), line.end() );
+        std::cout << line << std::endl;
+    }
+    
     return 0;
 }
